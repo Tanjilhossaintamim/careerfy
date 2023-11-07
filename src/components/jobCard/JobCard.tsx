c82fd0500ca5487f3d6c0521b16fdfa07cf5f5a9
Tanjil Hossain <tanjilhossain845@gmail.com>
2023-11-07 09:53:31 +0600

    compleate category section

---
+++ src/components/jobCard/JobCard.tsx
@@ -0,0 +1,67 @@
+import moment from "moment";
+interface JobInterface {
+  job: {
+    _id: string;
+    photoUrl: string;
+    jobTitle: string;
+    salaryRangeTo: string;
+    salaryRangeFrom: string;
+    jobApplicantsNumber: number;
+    jobPostingDate: string;
+    jobCategory: {
+      title: string;
+    };
+  };
+}
+const JobCard = ({ job }: JobInterface) => {
+  const {
+    photoUrl,
+    jobTitle,
+    jobCategory,
+    salaryRangeTo,
+    salaryRangeFrom,
+    jobApplicantsNumber,
+    jobPostingDate,
+  } = job;
+  const formatedTime = moment(jobPostingDate, "YYYYMMDD").fromNow();
+  let color = "";
+  if (jobCategory.title == "on-site") color = "#F1630D";
+  if (jobCategory.title == "hybrid") color = "#53B427";
+  if (jobCategory.title == "remote") color = "#186FC9";
+  if (jobCategory.title == "part-time") color = "#009CDF";
+  return (
+    <div className="max-w-[500px] border border-color-gray-1 flex items-center">
+      <div className="w-1/4 relative border-r border-color-gray-1 flex justify-center items-center">
+        <img src={photoUrl} alt="" className="w-full object-cover" />
+      </div>
+      <div className="w-full">
+        <div className="p-2 flex flex-col space-y-2">
+          <h1 className="text-[#333] font-semibold text-lg">{jobTitle}</h1>
+          <span
+            className={`p-1 inline-block w-fit text-sm text-white rounded`}
+            style={{ backgroundColor: color }}
+          >
+            {jobCategory.title}
+          </span>
+          <div className="flex items-center justify-between text-xs text-color-gray">
+            <h1 className="text-xs text-color-gray">
+              Published {formatedTime}
+            </h1>
+            <span>Applicants {jobApplicantsNumber}</span>
+          </div>
+          <div className="flex items-center py-3 justify-between border-t border-color-gray-1">
+            <h1 className="text-xs text-color-gray flex-grow">
+              Salary range &#2547;{salaryRangeFrom} - &#2547;{salaryRangeTo}
+            </h1>
+
+            <button className="bg-[#186FC9] text-white px-2 py-1 rounded">
+              view details
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
--- src/pages/home/components/category/Category.tsx
+++ src/pages/home/components/category/Category.tsx
@@ -1,9 +1,31 @@
 import Heading from "../../../../components/Heading/Heading";
-import { Tabs, TabsHeader, Tab } from "@material-tailwind/react";
+import { Tabs, TabsHeader, Tab, TabsBody } from "@material-tailwind/react";
 import { useGetCategoryQuery } from "../../../../redux/features/category/categoryApi";
+import { useGetAllJobsQuery } from "../../../../redux/features/jobs/jobApi";
+import { useState } from "react";
+import JobCard from "../../../../components/jobCard/JobCard";
 
+interface JobInterface {
+  _id: string;
+  photoUrl: string;
+  jobTitle: string;
+  salaryRangeTo: string;
+  salaryRangeFrom: string;
+  jobApplicantsNumber: number;
+  jobPostingDate: string;
+  jobCategory: {
+    title: string;
+  };
+}
 const Category = () => {
+  const [categoryValue, setCategoryValue] = useState("on-site");
+
   const { data: categories } = useGetCategoryQuery({});
+  const { data: jobs } = useGetAllJobsQuery({ category: categoryValue });
+
+  const handelFetchJobs = (category: string) => {
+    setCategoryValue(category);
+  };
 
   return (
     <div className="py-10">
@@ -13,16 +35,33 @@ const Category = () => {
           desc="A better career is out there. We'll help you find it. We're your first step to becoming everything you want to be."
         />
         <div className="my-6">
-          <Tabs id="custom-animation">
-            <TabsHeader>
-              {categories?.length > 0 &&
-                categories.map((tab: { title: string; _id: string }) => (
-                  <Tab key={tab._id} value={tab.title} className="capitalize">
-                    {tab.title}
-                  </Tab>
-                ))}
-            </TabsHeader>
-          </Tabs>
+          {categories?.length > 0 && (
+            <Tabs id="custom-animation" value="on-site">
+              <TabsHeader>
+                {categories?.length > 0 &&
+                  categories.map((tab: { title: string; _id: string }) => (
+                    <Tab
+                      key={tab._id}
+                      value={tab.title}
+                      onClick={() => handelFetchJobs(tab.title)}
+                      className="capitalize"
+                    >
+                      {tab.title}
+                    </Tab>
+                  ))}
+              </TabsHeader>
+
+              {/* tab body */}
+              <TabsBody>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-7">
+                  {jobs?.length > 0 &&
+                    jobs.map((job: JobInterface) => (
+                      <JobCard key={job._id} job={job} />
+                    ))}
+                </div>
+              </TabsBody>
+            </Tabs>
+          )}
         </div>
       </div>
     </div>
